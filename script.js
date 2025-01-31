e63e2ae01daa3f43d6fcd4942c5d71f363e6b800
Ashish Kumar <ashish73e@gmail.com>
2025-02-01 02:40:00 +0530

    added selection

--- script.js
+++ script.js
@@ -3,14 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
         let load = document.querySelector(".loadingScreen");
         load.style.opacity = "0";
         load.style.visibility = "hidden";
+
+
         setTimeout(function () {
             load.style.display = "none";
             let page = document.querySelector(".symbol");
             page.style.display = "block";
+
+
             setTimeout(() => {
                 page.style.opacity = "1";
             }, 100)
-            console.log("block");
+
+
         }, 1000);
     }, 1000)
 });
@@ -19,33 +24,51 @@ document.addEventListener("DOMContentLoaded", function () {
 let sign;
 
 let selected = document.querySelector(".choose");
-selected.addEventListener('click', (evt) => {
-    sign = evt.target.value;
+selected.addEventListener("click", (evt) => {
+    sign = evt.target.closest("button").value;
     let page = document.querySelector(".symbol");
     page.style.opacity = "0";
+
+
     setTimeout(function () {
-        page.style.display="none";
+        page.style.display = "none";
         let body = document.querySelector(".page");
         body.classList.remove(".load");
-        body.style.display="block";
-        setTimeout(()=>{
-            body.style.opacity="1";
-        },100)
+        body.style.display = "block";
+
+
+        setTimeout(() => {
+            body.style.opacity = "1";
+        }, 100);
+
+
     }, 1000);
-})
+});
+
+
+let computerSign;
+let code;
+if(sign === "cross") {
+    computerSign = "cross";
+    code = '<i class="fa-solid fa-xmark"></i>';
+}
+else {
+    computerSign = "circle";
+    code = '<i class="fa-regular fa-circle"></i>';
+};
+
 
 
 let clickedNodes = document.querySelectorAll(".box");
 
-
 clickedNodes.forEach((node) => {
     node.onclick = () => {
-        if (!node.querySelector(".circle")) {
-            let correct = document.createElement("div");
-            correct.classList.add("circle");
-            node.appendChild(correct);
+        if (!node.querySelector(".tick")) {
+            let tick = document.createElement("div");
+            tick.innerHTML = code;
+            tick.classList.add("tick");
+            node.append(tick);
         }
     }
 }
 );
-// let str ="<i class="fa-solid fa-xmark"></i>";
