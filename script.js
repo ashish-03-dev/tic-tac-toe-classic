659b0f93234c1a056bd7db643d19965c9edfbd15
Ashish Kumar <ashish73e@gmail.com>
2025-01-31 22:27:26 +0530

    added scoreboard

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 let clickedNodes = document.querySelectorAll(".box");
 
+
 clickedNodes.forEach((node) => {
     node.onclick = () => {
         if (!node.querySelector(".circle")){
-            let clicked = document.createElement("div");
-            clicked.classList.add("circle");
-            node.appendChild(clicked);
+            let correct = document.createElement("div");
+            correct.classList.add("circle");
+            node.appendChild(correct);
         }
     }
 }
 );
+// let str ="<i class="fa-solid fa-xmark"></i>";
